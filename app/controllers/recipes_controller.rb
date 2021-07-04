926cbf3870152f079092654112c82b1649098e76
sohey-dr <heyso1209@gmail.com>
2021-07-04 15:47:52 +0900

    Add RecipesController

---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,44 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :update, :destroy]
+
+  def index
+    recipes = Recipe.all
+    render json: { recipes: recipes }
+  end
+
+  def show
+    render json: { message: '"Recipe details by id"', recipe: @recipe }
+  end
+
+  def create
+    recipe = Recipe.new(recipe_params)
+    if recipe.save
+      render json: { message: "Recipe successfully created!" ,recipe: recipe }
+    else
+      render json: { message: "Recipe successfully created!", required: "title, making_time, serves, ingredients, cost" }
+    end
+  end
+
+  def destroy
+    if @recipe.destroy
+      render json: { message: 'Deleted the recipe', recipe: @recipe }
+    else
+      render json: { message: "No Recipe found" }
+    end
+  end
+
+  def update
+    @recipe.update(recipe_params)
+    render json: { message: '"Recipe successfully updated!"', recipe: @recipe }
+  end
+
+  private
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :making_time, :serves, :ingredients, :cost)
+  end
+end
