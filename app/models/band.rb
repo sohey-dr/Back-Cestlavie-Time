715fd005608b59abc80d27c549aaccd70dd669d6
soheyHashimoto <heyso1209@gmail.com>
2021-09-03 00:08:21 +0900

    [ADD]多対多でアソシエーション完了

--- app/models/band.rb
+++ app/models/band.rb
@@ -1,2 +1,4 @@
 class Band < ApplicationRecord
+  has_many :band_time_tables, dependent: :destroy
+  has_many :time_tables, through: :band_time_tables
 end
--- app/models/time_table.rb
+++ app/models/time_table.rb
@@ -1,3 +1,5 @@
 class TimeTable < ApplicationRecord
   belongs_to :live
+  has_many :band_time_tables, dependent: :destroy
+  has_many :bands, through: :band_time_tables
 end
